f8cebcb86965ad8e280a4f83c010e447142e40fc
pseudomuto <david.muto@gmail.com>
2016-03-30 19:59:12 -0400

    setting up targets, carthage and package.swift

---
+++ Sources/Retired.h
@@ -0,0 +1,15 @@
+//
+//  Retired.h
+//  Retired
+//
+//  Created by David Muto on 2016-03-30.
+//  Copyright © 2016 pseudomuto. All rights reserved.
+//
+
+@import Foundation
+
+//! Project version number for Retired iOS.
+FOUNDATION_EXPORT double RetiredVersionNumber;
+
+//! Project version string for Retired iOS.
+FOUNDATION_EXPORT const unsigned char RetiredVersionString[];
