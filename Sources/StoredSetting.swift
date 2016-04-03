83611a3562b8b9fe94a0fdcd368ba36925ea259e
pseudomuto <david.muto@gmail.com>
2016-04-03 11:20:00 -0400

    defining a StoredSetting object

---
+++ Sources/StoredSetting.swift
@@ -0,0 +1,37 @@
+//
+//  StoredSetting.swift
+//  Retired
+//
+//  Created by David Muto on 2016-04-03.
+//  Copyright © 2016 pseudomuto. All rights reserved.
+//
+
+public struct StoredSetting<T: AnyObject> {
+  private let key: String
+  private let settings: NSUserDefaults
+
+  public var value: T? {
+    get {
+      return settings.objectForKey(key) as? T
+    }
+    set {
+      if let setting = newValue {
+        settings.setObject(setting, forKey: key)
+      } else {
+        settings.removeObjectForKey(key)
+      }
+
+      settings.synchronize()
+    }
+  }
+
+  public init(_ name: String, container: NSUserDefaults = NSUserDefaults.standardUserDefaults()) {
+    key      = name
+    settings = container
+  }
+
+  public func clear() {
+    settings.removeObjectForKey(key)
+    settings.synchronize()
+  }
+}
---
+++ Tests/StoredSettingTests.swift
@@ -0,0 +1,55 @@
+//
+//  StoredSettingTests.swift
+//  Retired
+//
+//  Created by David Muto on 2016-04-03.
+//  Copyright © 2016 pseudomuto. All rights reserved.
+//
+
+import Retired
+import XCTest
+
+class StoredSettingTests: XCTestCase {
+  let settingKey        = "mySetting"
+  let settingsContainer = NSUserDefaults()
+
+  lazy var storedProp: StoredSetting<NSString> = {
+    return StoredSetting(self.settingKey, container: self.settingsContainer)
+  }()
+
+  override func tearDown() {
+    super.tearDown()
+
+    settingsContainer.removeObjectForKey(settingKey)
+    settingsContainer.synchronize()
+  }
+
+  func test_Get_Value_Pulls_From_Container() {
+    settingsContainer.setObject("My Value", forKey: settingKey)
+    XCTAssertEqual("My Value", storedProp.value)
+  }
+
+  func test_Get_Value_When_Nil_Returns_Nil() {
+    XCTAssertNil(storedProp.value)
+  }
+
+  func test_Set_Value_Stores_Value_In_Container() {
+    storedProp.value = "value"
+    XCTAssertEqual("value", settingsContainer.stringForKey(settingKey))
+  }
+
+  func test_Set_Value_To_Nil_Removes_Object_From_Container() {
+    storedProp.value = "value"
+    XCTAssertTrue(settingsContainer.dictionaryRepresentation().keys.contains(settingKey))
+
+    storedProp.value = nil
+    XCTAssertFalse(settingsContainer.dictionaryRepresentation().keys.contains(settingKey))
+  }
+
+  func test_Clear_Removes_Object_From_Container() {
+    storedProp.value = "value"
+    storedProp.clear()
+
+    XCTAssertFalse(settingsContainer.dictionaryRepresentation().keys.contains(settingKey))
+  }
+}
