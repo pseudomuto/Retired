3fdda6ed6830db1e3812ccd20103575e2c860311
Gabriel O'Flaherty-Chan <hi@gabrieloc.com>
2019-10-11 16:28:20 -0400

    adds product for spm

--- Package.swift
+++ Package.swift
@@ -1,3 +1,13 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
-let package = Package(name: "Retired")
+let package = Package(
+  name: "Retired",
+  products: [
+    .library(
+      name: "Retired iOS",
+      targets: ["Retired iOS"])
+  ]
+)
